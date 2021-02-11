8423927c10944ae3c3aa945a5a22ddc1e972db16
Igor Varshavsky <igor.varshavsky@gmail.com>
2021-02-11 11:53:59 -0500

    Refactoring to multimodule maven project. Closes #1.

--- pet-clinic-data/src/main/java/iva/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/iva/springframework/sfgpetclinic/model/Pet.java
@@ -15,6 +15,11 @@ public class Pet {
         return petType;
     }
 
+    /**
+     *
+     * @param petType
+     */
+
     public void setPetType(PetType petType) {
         this.petType = petType;
     }
