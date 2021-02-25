ee35fdce50635dc921eaca9c0842743a6b01a7b5
Igor Varshavsky <igor.varshavsky@gmail.com>
2021-02-25 16:31:22 -0500

    Added PetTypeSevice. Closes #29.

---
+++ pet-clinic-data/src/main/java/iva/springframework/sfgpetclinic/services/PetTypeService.java
@@ -0,0 +1,9 @@
+package iva.springframework.sfgpetclinic.services;
+
+import iva.springframework.sfgpetclinic.model.PetType;
+
+/**
+ * Created By iVa on 2/25/2021.
+ */
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
---
+++ pet-clinic-data/src/main/java/iva/springframework/sfgpetclinic/services/map/PetTypeMapService.java
@@ -0,0 +1,43 @@
+package iva.springframework.sfgpetclinic.services.map;
+
+import iva.springframework.sfgpetclinic.model.PetType;
+import iva.springframework.sfgpetclinic.services.PetTypeService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+/**
+ * Created By iVa on 2/25/2021.
+ */
+
+@Service
+public class PetTypeMapService extends AbstractMapService<PetType, Long> implements PetTypeService {
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+
+
+    @Override
+    public PetType save(PetType object) {
+        return super.save(object);
+    }
+
+    @Override
+    public void deleteBy(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType object) {
+    super.delete(object);
+    }
+
+
+
+}
